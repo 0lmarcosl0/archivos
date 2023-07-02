dd3470e0ff03476f6262a32c89a6cc7c3534c334
0lmarcosl0 <cuentainsana12uwu@gmail.com>
2023-07-02 20:38:39 -0300

    Tp2

---
+++ tp2.py
@@ -0,0 +1,98 @@
+#Pida un número al usuario y determine si es par o #impar
+
+#print("Par o Impar")
+#numero  = int(input("Dime un numeo: "))
+#impar_par = numero % 2
+#if impar_par == 0:
+#    print("Tu numero es par")
+#else:
+#    print("Tu numero es impar")
+
+#Escriba una cadena if-elif-else que determine el
+#estado de vida de una persona
+#print("Edad")
+#edad = int(input("Dime tu edad: "))
+
+#if edad < 0:
+#	print("ERROR")
+#	edad = int(input("Dime tu edad: "))
+#elif edad == 1:
+#	print("Eres un bebe")
+#elif edad  >=  2 and edad <4:
+#	print("Eres un infante")
+#elif edad  >= 4 and edad <12:
+#	print("Eres un niño")
+#elif edad  >= 13 and edad <20:
+#	print("Eres un adolecente")
+#elif edad  >= 20 and edad <65:
+#	print("Eres un adulto")
+#elif edad >= 65 and edad < 201:
+#	print("Eres un anciano")
+#else:
+#	print("ERROR")
+#	edad = int(input("Dime tu edad: "))
+
+#Cree un ciclo que nunca termine y ejecútelo. Puede probarlo haciendo que muestre algo en 
+
+#pantalla por cada pasada del ciclo. Para finalizarlo, #presione Ctrl-C o el comando para detener 
+
+#la ejecución correspondiente a su editor
+#while True:
+#	print("Arroz")
+
+#Escriba un programa que contenga dos ciclos while #anidados que muestre los enteros del 1
+#al 100, diez números por línea, como se muestra abajo
+#a = 1
+#b = 2
+#c = 3
+#d = 4
+#e = 5
+#f = 6
+#g = 7
+#h = 8
+#i = 9
+#j = 10
+#print(a , b , c , d , e , f , g , h ,i ,j)
+#while True:
+#	while j != 100:
+#		a = a + 10
+#		b = b + 10
+#		c = c + 10
+#		d =d + 10
+#		e = e + 10
+#		f = f + 10
+#		g = g + 10
+#		h = h + 10
+#		i = i +10
+#		j = j + 10
+#		print(a , b , c , d , e , f , g , h ,i ,j)
+#		if j == 100:
+#			break
+
+#Resuelva el ejercicio anterior usando solo un ciclo #while
+
+#a = 1
+#b = 2
+#c = 3
+#d = 4
+#e = 5
+#f = 6
+#g = 7
+#h = 8
+#i = 9
+#j = 10
+#print(a , b , c , d , e , f , g , h ,i ,j)
+#while True:
+#	a = a + 10
+#	b = b + 10
+#	c = c + 10
+#	d =d + 10
+#	e = e + 10
+#	f = f + 10
+#	g = g + 10
+#	h = h + 10
+#	i = i +10
+#	j = j + 10
+#	print(a , b , c , d , e , f , g , h ,i ,j)
+#	if j == 100:
+#		break
