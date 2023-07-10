152722ead1edf3cb9835c9802df1582632210bfb
0lmarcosl0 <cuentainsana12uwu@gmail.com>
2023-07-10 15:34:56 -0300

    Ta_te_ti.1.3

--- ta_te_ti.py
+++ ta_te_ti.py
@@ -1,5 +1,13 @@
-#no ay sitema de anti poner signo en otro
-
+#TA_TE_TI
+def gano(fila1, fila2, fila3, sym):
+    if fila1 == [sym, sym, sym] or fila2 == [sym, sym, sym] or fila3 == [sym, sym, sym]:
+        return True
+    elif fila1[0] == sym and fila2[0] == sym and fila3[0] == sym or fila1[1] == sym and fila2[1] == sym and fila3[1] == sym or fila1[2] == sym and fila2[2] == sym and fila3[2] == sym:
+        return True
+    elif fila1[0] == sym and fila2[1] == sym and fila3[2] == sym or fila1[2] == sym and fila2[1] == sym and fila3[0] == sym:
+        return True
+    else:
+        return False
 fila1 = ["   ","   ","   "]
 fila2 = ["   ","   ","   "]
 fila3 = ["   ","   ","   "]
@@ -9,8 +17,8 @@ print("Ta_te_ti")
 jugador1 = input("Símbolo del jugador 1: ")
 jugador2 = input("Símbolo del jugador 2: ")
 ganador = False
-
-while True:
+fin = 0
+while fin != 9:
     if turno == 1:
         print(f"Turno del jugador {turno} : {jugador1}")
         print("         1     2     3")
@@ -18,24 +26,32 @@ while True:
         print(f"b : {fila2}")
         print(f"c : {fila3}")
         cor_num = input("N_Dime la fila: ")
+        while cor_num != "1" and cor_num != "2" and cor_num != "3":
+            					print("ERROR")
+            					cor_num = input("N_Dime la fila: ")
         cor_letra = input("L_Dime cuál: ")
+        while cor_letra != "a" and cor_letra != "b" and cor_letra != "c":
+                  print("ERROR")
+                  cor_letra = input("L_Dime cuál: ")
         cor_num = int(cor_num)
         cor = cor_num - 1
-        if cor_num == cor_num and cor_letra == "a":
+        if cor_letra == "a":
             fila1.pop(cor)
             fila1.insert(cor, jugador1)
-        elif cor_num == cor_num and cor_letra == "b":
+        elif cor_letra == "b":
             fila2.pop(cor)
             fila2.insert(cor, jugador1)
-        elif cor_num == cor_num and cor_letra == "c":
+        elif cor_letra == "c":
             fila3.pop(cor)
-            fila3.insert(cor, jugador1)
-        
-        if fila1 == [jugador1, jugador1, jugador1] or fila2 == [jugador1, jugador1, jugador1] or fila3 == [jugador1, jugador1, jugador1]:
+            fila3.insert(cor, jugador1)        
+        if gano(fila1,fila2,fila3,jugador1):
             print(f"Ganador: Jugador 1")
             ganador = True
+            print(fila1)
+            print(fila2)
+            print(fila3)
             break
-        
+        fin += 1
         turno = 2
     else:
         print(f"Turno del jugador {turno} : {jugador2}")
@@ -44,26 +60,32 @@ while True:
         print(f"b : {fila2}")
         print(f"c : {fila3}")
         cor_num = input("N_Dime la fila: ")
+        while cor_num != "1" and cor_num != "2" and cor_num != "3":
+            					print("ERROR")
+            					cor_num = input("N_Dime la fila: ")
         cor_letra = input("L_Dime cuál: ")
+        while cor_letra != "a" and cor_letra != "b" and cor_letra != "c":
+                  print("ERROR")
+                  cor_letra = input("L_Dime cuál: ")
         cor_num = int(cor_num)
-        cor = cor_num - 1
-        
-        if cor_num == cor_num and cor_letra == "a":
+        cor = cor_num - 1        
+        if cor_letra == "a":
             fila1.pop(cor)
             fila1.insert(cor, jugador2)
-        elif cor_num == cor_num and cor_letra == "b":
+        elif cor_letra == "b":
             fila2.pop(cor)
             fila2.insert(cor, jugador2)
-        elif cor_num == cor_num and cor_letra == "c":
+        elif cor_letra == "c":
             fila3.pop(cor)
-            fila3.insert(cor, jugador2)
-        
-        if fila1 == [jugador2, jugador2, jugador2] or fila2 == [jugador2, jugador2, jugador2] or fila3 == [jugador2, jugador2, jugador2]:
+            fila3.insert(cor, jugador2)                
+        if gano(fila1,fila2,fila3,jugador2):
             print(f"Ganador: Jugador 2")
             ganador = True
+            print(fila1)
+            print(fila2)
+            print(fila3)
             break
-        
+        fin += 1
         turno = 1
-
 if not ganador:
     print("Empate")
